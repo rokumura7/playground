7a01e9056376bb152840974f8e9a9251506bb125
rokumura07 <rokumura.8807@gmail.com>
2020-07-14 19:24:13 +0900

    converted to English

--- src/main/java/com/rokumura/Judge.java
+++ src/main/java/com/rokumura/Judge.java
@@ -2,25 +2,27 @@ package com.rokumura;
 
 public class Judge {
   public void startJanken(Player p1, Player p2) {
-    System.out.println("3回勝負！ジャンケンを開始します！");
+    System.out.println("Let’s play stone-paper-scissors");
     for (int i = 1; i <= 3; i++) {
-      System.out.println(i + "回目！");
+      System.out.println("\nRound " + i);
       Player winner = judgeJanken(p1, p2);
       if (winner != null) {
         winner.notifyResult(true);
+        System.out.println(winner.getName() + " won.");
       } else {
-        System.out.println("引き分け！");
+        System.out.println("draw");
       }
     }
 
+    System.out.println("------------------");
     System.out.println(p1.getName() + ":" + p1.getWinCount());
     System.out.println(p2.getName() + ":" + p2.getWinCount());
     if (p1.getWinCount() > p2.getWinCount()) {
-      System.out.println(p1.getName() + "の勝ち！");
+      System.out.println(p1.getName() + " won.");
     } else if (p2.getWinCount() > p1.getWinCount()) {
-      System.out.println(p2.getName() + "の勝ち！");
+      System.out.println(p2.getName() + " won.");
     } else {
-      System.out.println("引き分け！");
+      System.out.println("draw");
     }
   }
 
