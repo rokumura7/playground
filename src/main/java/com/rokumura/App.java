52d2d332a52aba16aaf9380249716c13d2a92f41
rokumura07 <rokumura.8807@gmail.com>
2020-07-16 20:03:57 +0900

    fixed recruting players

--- src/main/java/com/rokumura/App.java
+++ src/main/java/com/rokumura/App.java
@@ -2,17 +2,25 @@ package com.rokumura;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class App {
   public static void main( String[] args) {
     Judge judge = new Judge();
 
-    List<Player> players = new ArrayList<>();
-    players.add(new Player("John", new RandomTactics()));
-    players.add(new Player("Mike", new RandomTactics()));
-    players.add(new Player("Alex", new RandomTactics()));
-    players.add(new Player("Paul", new RandomTactics()));
-
+    List<Player> players = recruitPlayers();
+    if (players.size() < 2) throw new RuntimeException("Lack of players...");
     judge.startJanken(players);
+  }
+
+  private static List<Player> recruitPlayers() {
+    String[] people = {"Jacob","Sophia","Mason","Emma","Ethan","Isabella"};
+    Random random = new Random();
+    int randomNum = random.nextInt(people.length);
+    List<Player> players = new ArrayList<>();
+    for (int i = 0; i < randomNum; i++) {
+      players.add(new Player(people[i], new RandomTactics()));
+    }
+    return players;
   }
 }
