cca4b70aeab46b98c54c9f99d1944440fac28240
rokumura07 <rokumura.8807@gmail.com>
2020-07-18 01:20:23 +0900

    added test cases for Judge.class

--- src/test/java/com/rokumura/JudgeTest.java
+++ src/test/java/com/rokumura/JudgeTest.java
@@ -11,6 +11,28 @@ import org.junit.*;
 import java.util.*;
 
 public class JudgeTest {
+
+  @Test
+  public void testJudgeJanken() throws Exception {
+    Player p1 = new Player("winner", new StoneTactics());
+    Player p2 = new Player("loser", new ScissorsTactics());
+    Player p3 = new Player("loser", new ScissorsTactics());
+    Player p4 = new Player("loser", new ScissorsTactics());
+    List<Player> players = new ArrayList<>();
+    players.add(p1);
+    players.add(p2);
+    players.add(p3);
+    players.add(p4);
+
+    players.stream().forEach(p -> p.decideHand());
+
+    Judge judge = new Judge();
+    Method judgeJanken = Judge.class.getDeclaredMethod("judgeJanken", List.class);
+    judgeJanken.setAccessible(true);
+
+    assertEquals("winner is p1", Arrays.asList(p1), judgeJanken.invoke(judge, players));
+  }
+
   @Test
   public void testJudgeResult() throws Exception {
     Player p1 = new Player("winner", new RandomTactics());
