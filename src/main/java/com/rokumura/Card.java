3b310a093ab808449017f87c6e765447b3ce770a
rokumura07 <rokumura.8807@gmail.com>
2020-07-21 20:35:09 +0900

    created Card.java

---
+++ src/main/java/com/rokumura/Card.java
@@ -0,0 +1,54 @@
+package com.rokumura;
+
+public class Card {
+  private Suit suit;
+  private int number;
+
+  public Card(Suit suit, int number) {
+    this.suit = suit;
+    this.number = number;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    switch (suit) {
+      case SPADE:
+        sb.append("♠️");
+        break;
+      case HEART:
+        sb.append("❤︎");
+        break;
+      case DIAMOND:
+        sb.append("♦︎");
+        break;
+      case CLUB:
+        sb.append("☘");
+        break;
+      case JOKER:
+        return "🃏";
+    }
+    switch (number) {
+      case 1:
+        sb.append("A");
+        break;
+      case 11:
+        sb.append("J");
+        break;
+      case 12:
+        sb.append("Q");
+        break;
+      case 13:
+        sb.append("K");
+        break;
+      default:
+        sb.append(number);
+        break;
+    }
+    return sb.toString();
+  }
+}
