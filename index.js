727b65602ab7c2e135749327a5f7169074696c2a
rokumura07 <rokumura.8807@gmail.com>
2020-08-03 20:32:23 +0900

    getting tr-Items

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  console.log('Hello')
   const browser = await puppeteer.launch()
-  await browser.newPage()
-    .then(page => page.goto('https://qiita.com/'))
-    .then(page => console.log(page.status()))
+  const page = await browser.newPage()
+  const qiita = await goto(page, 'https://qiita.com/')
+  const articles = await qiita.$$('div.tr-Item')
+  for (let i = 1; i <= articles.length; i++) {
+    const like = await page.$eval(`div.tr-Item:nth-child(${i}) .tr-Item_likeCount`, elm => elm.textContent)
+    console.log(like)
+    const title = await page.$eval(`div.tr-Item:nth-child(${i}) .tr-Item_title`, elm => elm.textContent)
+    console.log(title)
+  }
   await browser.close()
 })();
+
+goto = async (page, url) => {
+  await page.goto(url)
+  await page.waitFor(1000)
+  return page
+}
