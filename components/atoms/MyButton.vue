02a9cda8763abfeacfcabf21e5de664b9c199b31
rokumura7 <rokumura.8807@gmail.com>
2020-10-23 22:26:38 +0900

    refactor

--- components/atoms/MyButton.vue
+++ components/atoms/MyButton.vue
@@ -9,9 +9,7 @@
 
 <script lang="ts">
 import Vue, { PropOptions } from 'vue'
-import Colors from '../../mixins/Colors'
-
-type Color = 'default' | 'primary' | 'success' | 'warning' | 'danger'
+import { Color, getColorSet } from '../../mixins/Colors'
 
 export default Vue.extend({
   name: 'MyButton',
@@ -29,30 +27,8 @@ export default Vue.extend({
   },
   computed: {
     getColor(): String {
-      const styles = []
-      switch (this.color) {
-        case 'primary':
-          styles.push(Colors.bg.primary)
-          styles.push(Colors.bgHover.primary)
-          break
-        case 'success':
-          styles.push(Colors.bg.success)
-          styles.push(Colors.bgHover.success)
-          break
-        case 'warning':
-          styles.push(Colors.bg.warning)
-          styles.push(Colors.bgHover.warning)
-          break
-        case 'danger':
-          styles.push(Colors.bg.danger)
-          styles.push(Colors.bgHover.danger)
-          break
-        default:
-          styles.push(Colors.bg.default)
-          styles.push(Colors.bgHover.default)
-          break
-      }
-      return styles.join(' ')
+      const colorSet = getColorSet(this.color)
+      return [colorSet.bg, colorSet.bgHover].join(' ')
     },
   },
 })
--- mixins/Colors.ts
+++ mixins/Colors.ts
@@ -1,29 +1,38 @@
+export type Color = 'default' | 'primary' | 'success' | 'warning' | 'danger'
+export const getColorSet = (color: Color): ColorSet => colors[color]
+
 interface ColorSet {
-  primary: string
-  success: string
-  warning: string
-  danger: string
-  default: string
+  bg: string
+  bgHover: string
 }
 
-export interface Colors {
-  bg: ColorSet
-  bgHover: ColorSet
+interface Colors {
+  primary: ColorSet
+  success: ColorSet
+  warning: ColorSet
+  danger: ColorSet
+  default: ColorSet
 }
 
-export default {
-  bg: {
-    primary: 'bg-indigo-500',
-    success: 'bg-green-500',
-    warning: 'bg-yellow-500',
-    danger: 'bg-red-500',
-    default: 'bg-gray-500',
+const colors = {
+  primary: {
+    bg: 'bg-indigo-500',
+    bgHover: 'hover:bg-indigo-600',
   },
-  bgHover: {
-    primary: 'hover:bg-indigo-600',
-    success: 'hover:bg-green-600',
-    warning: 'hover:bg-yellow-600',
-    danger: 'hover:bg-red-600',
-    default: 'hover:bg-gray-600',
+  success: {
+    bg: 'bg-green-500',
+    bgHover: 'hover:bg-green-600',
+  },
+  warning: {
+    bg: 'bg-yellow-500',
+    bgHover: 'hover:bg-yellow-600',
+  },
+  danger: {
+    bg: 'bg-red-500',
+    bgHover: 'hover:bg-red-600',
+  },
+  default: {
+    bg: 'bg-gray-500',
+    bgHover: 'hover:bg-gray-600',
   },
 } as Colors
