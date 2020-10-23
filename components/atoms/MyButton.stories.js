075eb86613c3608a4d788edafc68b6d673a6ea50
rokumura7 <rokumura.8807@gmail.com>
2020-10-23 22:11:06 +0900

    add MyButton

---
+++ components/atoms/MyButton.stories.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'Button',
+}
+
+export const Default = () => '<MyButton label="default" />'
+export const Primary = () => '<MyButton label="primary" color="primary" />'
+export const Success = () => '<MyButton label="success" color="success" />'
+export const Warning = () => '<MyButton label="warning" color="warning" />'
+export const Danger = () => '<MyButton label="danger" color="danger" />'
---
+++ components/atoms/MyButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    class="text-white uppercase font-bold py-1 px-2 rounded"
+    :class="[getColor]"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import Colors from '../../mixins/Colors'
+
+type Color = 'default' | 'primary' | 'success' | 'warning' | 'danger'
+
+export default Vue.extend({
+  name: 'MyButton',
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    color: {
+      type: String,
+      required: false,
+      default: 'default',
+    } as PropOptions<Color>,
+  },
+  computed: {
+    getColor(): String {
+      const styles = []
+      switch (this.color) {
+        case 'primary':
+          styles.push(Colors.bg.primary)
+          styles.push(Colors.bgHover.primary)
+          break
+        case 'success':
+          styles.push(Colors.bg.success)
+          styles.push(Colors.bgHover.success)
+          break
+        case 'warning':
+          styles.push(Colors.bg.warning)
+          styles.push(Colors.bgHover.warning)
+          break
+        case 'danger':
+          styles.push(Colors.bg.danger)
+          styles.push(Colors.bgHover.danger)
+          break
+        default:
+          styles.push(Colors.bg.default)
+          styles.push(Colors.bgHover.default)
+          break
+      }
+      return styles.join(' ')
+    },
+  },
+})
+</script>
