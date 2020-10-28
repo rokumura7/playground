a43f7b7acbd7ceb40c1cc13006e0c4a69a6f8ae1
rokumura7 <rokumura.8807@gmail.com>
2020-10-28 21:33:52 +0900

    factory method pattern framework

---
+++ src/main/java/factory_method/framework/Factory.java
@@ -0,0 +1,11 @@
+package factory_method.framework;
+
+public abstract class Factory {
+  public final Product create(String owner) {
+    Product p = createProduct(owner);
+    registerProduct(p);
+    return p;
+  }
+  protected abstract Product createProduct(String string);
+  protected abstract void registerProduct(Product product);
+}
