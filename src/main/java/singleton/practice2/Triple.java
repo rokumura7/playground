9cf9919f0c5e21dfcb043bc06beae504cd92d622
rokumura7 <rokumura.8807@gmail.com>
2020-11-01 23:06:58 +0900

    singleton pattern practice2

---
+++ src/main/java/singleton/practice2/Triple.java
@@ -0,0 +1,21 @@
+package singleton.practice2;
+
+public class Triple {
+  private static Triple[] triples = new Triple[] {
+          new Triple(0), new Triple(1), new Triple(2)
+  };
+  private int id;
+  private Triple(int id) {
+    this.id = id;
+  }
+  public static Triple getInstance(int id) {
+    return triples[id];
+  }
+
+  @Override
+  public String toString() {
+    return "Triple{" +
+            "id=" + id +
+            '}';
+  }
+}
