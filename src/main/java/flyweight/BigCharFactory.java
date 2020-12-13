cc04b1b2ed1940aa84522f6beb582d6e712a2b2d
rokumura7 <rokumura.8807@gmail.com>
2020-12-13 14:23:32 +0900

    flyweight pattern

---
+++ src/main/java/flyweight/BigCharFactory.java
@@ -0,0 +1,23 @@
+package flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BigCharFactory {
+    private Map<String, BigChar> pool = new HashMap<>();
+    private static BigCharFactory singleton = new BigCharFactory();
+    private BigCharFactory() {}
+
+    public static BigCharFactory getInstance() {
+        return singleton;
+    };
+
+    public synchronized BigChar getBigChar(char charname) {
+        BigChar bc = pool.get("" + charname);
+        if (bc == null) {
+            bc = new BigChar(charname);
+            pool.put("" + charname, bc);
+        }
+        return bc;
+    }
+}
