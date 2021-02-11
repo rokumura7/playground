0c16dbe5214471735ff74257323168475ab5d9d0
rokumura7 <rokumura.8807@gmail.com>
2021-02-12 07:04:37 +0900

    fix ts_puppeteer

--- ts_puppeteer/src/index.ts
+++ ts_puppeteer/src/index.ts
@@ -3,14 +3,21 @@ import puppeteer from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://qiita.com/');
+  await page.goto('https://rokumura7.github.io/');
 
   console.log(await page.title());
 
-  const articles = await page.$$('article > h2 > a');
-  for (const article of articles) {
-    const title = await page.evaluate((elm) => elm.innerText, article);
-    console.log(title);
+  await Promise.all([
+    page.click('header > nav > div > a:nth-child(2)'),
+    page.waitForNavigation(),
+  ]);
+
+  const items = await page.$$(
+    '#__next > div > div > div.absolute.pt-20.inset-0.h-full.min-w-387 > div > div > div.flex.justify-center.mb-5 > ul > li:nth-child(1) > div > div > p'
+  );
+  for (const item of items) {
+    const text = await page.evaluate((elm) => elm.innerText, item);
+    console.log(text);
   }
 
   await browser.close();
