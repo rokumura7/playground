b88ec4bf0344501392cda633ba09579a381ad2fa
rokumura7 <rokumura.8807@gmail.com>
2021-02-12 06:57:54 +0900

    add lint and prettier for ts_puppeteer

--- ts_puppeteer/src/index.ts
+++ ts_puppeteer/src/index.ts
@@ -1,17 +1,17 @@
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://qiita.com/')
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://qiita.com/');
 
-  console.log(await page.title())
+  console.log(await page.title());
 
-  const articles = await page.$$('article > h2 > a')
+  const articles = await page.$$('article > h2 > a');
   for (const article of articles) {
-    const title = await page.evaluate(elm => elm.innerText, article)
-    console.log(title)
+    const title = await page.evaluate((elm) => elm.innerText, article);
+    console.log(title);
   }
 
-  await browser.close()
+  await browser.close();
 })();
