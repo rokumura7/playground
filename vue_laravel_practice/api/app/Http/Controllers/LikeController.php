22a11d2f63183e4a5d23ee38b8f8a77c45bd6765
rokumura7 <rokumura.8807@gmail.com>
2021-02-14 17:36:16 +0900

    make rest apis

--- vue_laravel_practice/api/app/Http/Controllers/LikeController.php
+++ vue_laravel_practice/api/app/Http/Controllers/LikeController.php
@@ -2,62 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function like(Request $request) {
+        $like = new Like();
+        $like->user_id = $request->user_id;
+        $like->post_id = $request->post_id;
+        $like->save();
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    
+    public function unlike(Request $request) {
+        $target = Like::where('user_id', $request->user_id)
+            ->where('post_id', $request->post_id)
+            ->first();
+        $target->delete();
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    
+    public function destroy($id) {
+        Like::find($id)->delete();
     }
 }
--- vue_laravel_practice/api/app/Http/Controllers/PostController.php
+++ vue_laravel_practice/api/app/Http/Controllers/PostController.php
@@ -2,62 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function index() {
+        $posts = Post::with('user')
+            ->with('likes')
+            ->with('comments')
+            ->orderby('created_at', 'desc')
+            ->get();
+        return compact('posts');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
+        $post = new Post();
+        $post->content = $request->content;
+        $post->user_id = $request->user_id;
+        $post->save();
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    
+    public function destroy($id) {
+        Post::find($id)->delete();
     }
 }
