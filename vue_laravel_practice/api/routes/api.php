4e4be298c3dc620f847fe0a804fa8987276d0049
rokumura7 <rokumura.8807@gmail.com>
2021-02-14 17:45:56 +0900

    make routes for api

--- vue_laravel_practice/api/routes/api.php
+++ vue_laravel_practice/api/routes/api.php
@@ -1,18 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->prefix('v1')->group(function() {
+    Route::post('signUp', 'UserController@store');
+    Route::post('login',  'UserController@login');
+    Route::get('logout',  'UserController@logout')->middleware('jwt.refresh');
+    Route::get('users',   'UserController@index');
+    
+    Route::middleware('jwt.auth')->group(function() {
+        Route::resource('users',     'UserController', 
+            array('except' => array('index', 'create', 'show', 'edit', 'update')));
+        Route::resource('posts',     'PostController', 
+            array('except' => array('show', 'create', 'edit', 'update')));
+        Route::resource('comments',  'CommentController', 
+            array('except' => array('show', 'create', 'edit', 'update')));
+        Route::post('like',          'LikeController@like');
+        Route::post('unlike',        'LikeController@unlike');
+        Route::delete('likes/{id}',  'LikeController@destroy');
+        // 管理者向けAPI
+        Route::get('fetch_users',    'UserController@fetch');
+        Route::get('fetch_posts',    'PostController@fetch');
+        Route::get('fetch_likes',    'LikeController@fetch');
+        Route::get('fetch_comments', 'CommentController@fetch');
+    });
 });
