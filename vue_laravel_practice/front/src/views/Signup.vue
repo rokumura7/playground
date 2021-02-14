1261cc78bb20385088b8ee49a813e03e4159c514
rokumura7 <rokumura.8807@gmail.com>
2021-02-14 20:39:47 +0900

    remove unused files

--- vue_laravel_practice/front/src/views/Signup.vue
+++ vue_laravel_practice/front/src/views/Signup.vue
@@ -1,14 +1,11 @@
 <template>
-  <div>Hello World.</div>
-  <HelloWorld msg="Hi!" />
+  <div>This is signup.</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'Signup',
-  components: HelloWorld,
 })
 </script>
