3b016355f6e340a903ae95855b651fc0b9c0f67c
rokumura7 <rokumura.8807@gmail.com>
2021-02-15 21:10:55 +0900

    add MyButton.vue

---
+++ vue_laravel_practice/front/src/components/atoms/MyButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="px-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+    @click="onClick"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MyButton',
+  props: {
+    label: {
+      type: String,
+      default: "I'm button!",
+    },
+  },
+})
+</script>
---
+++ vue_laravel_practice/front/src/components/atoms/index.ts
@@ -0,0 +1,3 @@
+import MyButton from '@/components/atoms/MyButton.vue'
+
+export { MyButton }
