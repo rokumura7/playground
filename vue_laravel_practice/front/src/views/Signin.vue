aa43048e42ec2c44b3178cb74a7f2b1467477f13
rokumura7 <rokumura.8807@gmail.com>
2021-02-27 21:00:22 +0900

    implement login feature

--- vue_laravel_practice/front/src/views/Signin.vue
+++ vue_laravel_practice/front/src/views/Signin.vue
@@ -4,13 +4,21 @@
       <CardHeader label="SIGN IN" />
       <form class="p-5 space-y-5">
         <div>
-          <MyInput type="email" placeholder="email" />
+          <MyInput
+            type="email"
+            placeholder="email"
+            @change-value="changeEmail"
+          />
         </div>
         <div>
-          <MyInput type="password" placeholder="password" />
+          <MyInput
+            type="password"
+            placeholder="password"
+            @change-value="changePassword"
+          />
         </div>
         <div class="text-right">
-          <MyButton label="SIGN IN" />
+          <MyButton label="SIGN IN" @click="signin" />
         </div>
         <div class="text-right border-t pt-5">
           <router-link to="/signup"><MyButton label="SIGN UP" /></router-link>
@@ -21,11 +29,36 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
+import { useRouter } from 'vue-router'
 import { MyButton, MyInput, CardHeader } from '@/components/atoms'
+import { post } from '@/helpers/Http'
 
 export default defineComponent({
   name: 'Signin',
   components: { MyButton, MyInput, CardHeader },
+  setup() {
+    const router = useRouter()
+    const state = reactive({
+      email: '',
+      password: '',
+    })
+
+    const changeEmail = (email: string) => (state.email = email)
+    const changePassword = (password: string) => (state.password = password)
+
+    const signin = async () => {
+      const res = await post('login', state)
+      if (res.status === 200) {
+        router.push('/home')
+      }
+    }
+    return {
+      state,
+      changeEmail,
+      changePassword,
+      signin,
+    }
+  },
 })
 </script>
